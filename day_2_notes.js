ea95ef399a688c1d49027ef800eb1108c61a1118
Hanna <Bethany.Hanna@autofinance.com>
2021-03-25 20:39:53 -0400

    Initialize class notes repo

---
+++ day_2_notes.js
@@ -0,0 +1,15 @@
+// Git
+// Version control system designed to keep chronological timeline of all of your code changes.
+/*To travers file system in terminal:
+cd name_of_folder moves you down
+cd .. moves you up
+ls lists items inside the folder
+pwd specifies directory path you're currently in
+
+To initiate a git repository
+1) git init
+2) Staging files for a commit - git add <file_name> OR git add . (to add all files in folder)
+git status - tells us the status of our repository
+git reset can convert back before committing to the repository if for some reason we don't want to commit all the files
+git commit -m "type commit message here to provide short explanation of changes made"
+*/
